c1ff732e3f1d711b35805405016c5e2bcf5b0ec3
sayeed shaik <33728316+sayeedm44@users.noreply.github.com>
2024-10-30 08:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 const users = [
   { username: "admin", password: "Brio@2024#*" },
   { username: "sayeed", password: "Brio@2024#*" }, // Add more users as needed
-  { username: "afash", password: "Brio@2024#*" }
-  { username: "venkatesh", password: "Brio@2024#*" }
-  { username: "mallesh", password: "Brio@2024#*" }
-  { username: "ibrahim", password: "Brio@2024#*" }
-  { username: "nandhini", password: "Brio@2024#*" }
-  { username: "venu", password: "Brio@2024#*" }
-  { username: "vinod", password: "Brio@2024#*" }
-  { username: "haji", password: "Brio@2024#*" }
-  { username: "vignesh", password: "Brio@2024#*" }
+  { username: "afash", password: "Brio@2024#*" }, // Comma added here
+  { username: "venkatesh", password: "Brio@2024#*" }, // Comma added here
+  { username: "mallesh", password: "Brio@2024#*" }, // Comma added here
+  { username: "ibrahim", password: "Brio@2024#*" }, // Comma added here
+  { username: "nandhini", password: "Brio@2024#*" }, // Comma added here
+  { username: "venu", password: "Brio@2024#*" }, // Comma added here
+  { username: "vinod", password: "Brio@2024#*" }, // Comma added here
+  { username: "haji", password: "Brio@2024#*" }, // Comma added here
+  { username: "vignesh", password: "Brio@2024#*" } // Last object doesn't need a comma
 ];
 
 function login() {
@@ -29,4 +29,3 @@ function login() {
     errorMessage.textContent = "Invalid username or password";
   }
 }
-
